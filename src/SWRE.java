e4bf157b5e756616fd52e56485a61daa5a3a0362
Amauzo <Amauzo@users.noreply.github.com>
2020-10-21 16:41:40 +0200

    Idea 2

--- src/SWRE.java
+++ src/SWRE.java
@@ -1,56 +1,61 @@
+import javafx.util.StringConverter;
+
 import java.awt.event.ActionEvent;
-import java.util.Scanner;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 /** version 0.01**/
-
+//0.00 --> FAIL Scanner uniquement à oublié impossible d'avoir les types de substat
+//0.01 --> FAIL Bouton à améliorer
 public class SWRE {
+
+    private static String TypeRunes;
+    private static String[] types_runes ={
+            "Endurance","Swift","Violence","Rage" //A COMPLETER
+    };
+
     public static void  main(String[] args){
-        //System.out.println("hello WORLD !");
-        //Scanner uniquement à oublié impossible d'avoir les types de substat
-        /**
-        try ( Scanner scanner = new Scanner( System.in ) ) {
-            System.out.print( "Please enter the main stats : " );
-            int mainstat = scanner.nextInt();
-            System.out.print( "Please enter the initial stats : " );
-            int initstat = scanner.nextInt();
-            System.out.print( "Please enter the first sub-stat : " );
-            int ss1 = scanner.nextInt();
-            System.out.print( "Please enter the first sub-stat : " );
-            int ss2 = scanner.nextInt();
-            System.out.print( "Please enter the first sub-stat : " );
-            int ss3 = scanner.nextInt();
-            System.out.print( "Please enter the first sub-stat : " );
-            int ss4 = scanner.nextInt();
-            int result = a + b;
-            System.out.printf( "La somme de %d et de %d vaut %d\n", a, b, result );
-        }**/
         JFrame frame = new JFrame("SW Runes Efficiency");
         JButton button = new JButton("Compute efficiency");
-        button.setSize(100,100);
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel typeRune = new JLabel("amauzo");
+        button.setBounds(0,0,250,40);
+        typeRune.setBounds(150,150,200,40);
         frame.add(button);
+        frame.add(typeRune);
         frame.setSize(500,450);
+        frame.setLayout(null);
         frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        button.addActionListener(new java.awt.event.ActionListener() {
+         @Override
+            /**public void actionPerformed(java.awt.event.ActionEvent evt) {
+                 JOptionPane.showMessageDialog(null, new JList(types_runes));
+              TypeRunes = JOptionPane;
+             String name = JOptionPane.showInputDialog(frame,
+                         TypeRunes, null);
+             }**/
+            public void actionPerformed(ActionEvent evt) {
+             //Calcul de l'efficiency
+             // Exemple a refaire
 
-        /**Ajouter une pop-up au lancement demandant les inforamtions essentiel :Type et valeurs des stats
-         *
-         *public void actionPerformed(ActionEvent e)
-         *     {
-         *         p.show();
-         *     }
-         button.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                String name = JOptionPane.showInputDialog(frame,
-                        "What is your name?", null);
-
+             //String TRune;
+             //TRune = JOptionPane.showInputDialog(null, "Types of runes : ","Rune Type Selector");
+             //TRune = JOptionPane.showMessageDialog(null, new //Jlist(types_runes));
+             //typeRune.setText(TRune);
             }
-        });**/
+        });
+
     }
+
+    /**Ajouter une pop-up au lancement demandant les inforamtions essentiel :Type et valeurs des stats
+     *
+     *public void actionPerformed(ActionEvent e)
+     *     {
+     *         p.show();
+     *     }
+     * });
+     **/
+
 
 }
